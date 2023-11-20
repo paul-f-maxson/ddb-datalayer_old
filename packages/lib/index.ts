a22d56c6f7c3f9848c01faac317244ce3f320f83
paul-f-maxson <paul.f.maxson@gmail.com>
2023-11-20 17:02:13 -0500

    buggy but works

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,24 +1,45 @@
 import z from "zod";
-import * as lodash from "lodash";
+import lodash from "lodash";
 import { Paths, Get } from "type-fest";
 
-const get =
-  <S extends z.ZodType<any>>(schema: S) =>
-  <BaseType = z.infer<S>>(object: BaseType) =>
-  <Path extends Exclude<Paths<BaseType>, number>>(
-    path: Path,
-  ) =>
-    lodash.get(schema.parse(object), path) as Get<
-      BaseType,
-      Path
-    >;
+type AllowedGSIs = [never, "gsi1", "gsi1" | "gsi2", "gsi1" | "gsi2" | "gsi3", "gsi1" | "gsi2" | "gsi3" | "gsi4", "gsi1" | "gsi2" | "gsi3" | "gsi4" | "gsi5" ]
 
-const user = z.object({ username: z.string() });
+export const createTable = (globalSecondaryIndices: 0 | 1 | 2 | 3 | 4 | 5) => ({
+  createSchema: <S extends z.ZodType<any>, BaseType extends z.infer<S>, Path extends Exclude<Paths<BaseType>, number>>(documentSchema: S,
+      keyPaths: {
+        pk: Path,
+        sk?: Path,
+        secondaryIndexPaths?: {[gsiPath in AllowedGSIs[typeof globalSecondaryIndices]]?: {pk: Path, sk?: Path}}
+      },
+    ) => ({
+     makeEntry: (document: BaseType) => ({
+      document,
+      pk: 
+        lodash.get(documentSchema.parse(document), keyPaths.pk) as Get<
+          BaseType,
+          Path
+        >,
+      sk: 
+        keyPaths.sk ? lodash.get(documentSchema.parse(document), keyPaths.sk) as Get<
+          BaseType,
+          Path
+        > : undefined,
+      secondaryIndices: keyPaths.secondaryIndexPaths? Object.entries(keyPaths.secondaryIndexPaths).reduce((acc, [indexName, {pk, sk}]) => ({...acc, [indexName]: {
+        pk: 
+        lodash.get(documentSchema.parse(document), pk) as Get<
+          BaseType,
+          Path
+        >,
+      sk: 
+        sk ? lodash.get(documentSchema.parse(document), sk) as Get<
+          BaseType,
+          Path
+        > : undefined,
+      }}), {}) : undefined
+      }),
+      
+  })});
 
-user.parse({
-  username: "paul",
-});
+  // ERROR: gsi2 should bot be allowed here
+  createTable(1).createSchema(z.object({userName: z.string(), age: z.number()}), {pk: "userName", secondaryIndexPaths: {gsi1: {pk: "age"}, gsi2: {pk: "age"}}})
 
-get(user)({
-  username: "paul",
-})("username");
