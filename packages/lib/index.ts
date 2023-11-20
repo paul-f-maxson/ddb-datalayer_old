8ca914e78913ca5780ddc345f9592101c9792f9c
paul-f-maxson <paul.f.maxson@gmail.com>
2023-11-19 19:33:13 -0500

    rough typed getter

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,1 +1,24 @@
-export default {};
+import z from "zod";
+import * as lodash from "lodash";
+import { Paths, Get } from "type-fest";
+
+const get =
+  <S extends z.ZodType<any>>(schema: S) =>
+  <BaseType = z.infer<S>>(object: BaseType) =>
+  <Path extends Exclude<Paths<BaseType>, number>>(
+    path: Path,
+  ) =>
+    lodash.get(schema.parse(object), path) as Get<
+      BaseType,
+      Path
+    >;
+
+const user = z.object({ username: z.string() });
+
+user.parse({
+  username: "paul",
+});
+
+get(user)({
+  username: "paul",
+})("username");
