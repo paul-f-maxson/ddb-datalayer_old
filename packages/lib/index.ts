0442d6aa2804a650971d547d1496d4193e26ed46
paul-f-maxson <paul.f.maxson@gmail.com>
2023-11-24 15:34:04 -0500

    bug fixes

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -2,44 +2,78 @@ import z from "zod";
 import lodash from "lodash";
 import { Paths, Get } from "type-fest";
 
-type AllowedGSIs = [never, "gsi1", "gsi1" | "gsi2", "gsi1" | "gsi2" | "gsi3", "gsi1" | "gsi2" | "gsi3" | "gsi4", "gsi1" | "gsi2" | "gsi3" | "gsi4" | "gsi5" ]
+type AllowedGSIs = [
+  keyof {},
+  "gsi1",
+  "gsi1" | "gsi2",
+  "gsi1" | "gsi2" | "gsi3",
+  "gsi1" | "gsi2" | "gsi3" | "gsi4",
+  "gsi1" | "gsi2" | "gsi3" | "gsi4" | "gsi5",
+];
 
-export const createTable = (globalSecondaryIndices: 0 | 1 | 2 | 3 | 4 | 5) => ({
-  createSchema: <S extends z.ZodType<any>, BaseType extends z.infer<S>, Path extends Exclude<Paths<BaseType>, number>>(documentSchema: S,
-      keyPaths: {
-        pk: Path,
-        sk?: Path,
-        secondaryIndexPaths?: {[gsiPath in AllowedGSIs[typeof globalSecondaryIndices]]?: {pk: Path, sk?: Path}}
-      },
-    ) => ({
-     makeEntry: (document: BaseType) => ({
-      document,
-      pk: 
-        lodash.get(documentSchema.parse(document), keyPaths.pk) as Get<
-          BaseType,
-          Path
-        >,
-      sk: 
-        keyPaths.sk ? lodash.get(documentSchema.parse(document), keyPaths.sk) as Get<
-          BaseType,
-          Path
-        > : undefined,
-      secondaryIndices: keyPaths.secondaryIndexPaths? Object.entries(keyPaths.secondaryIndexPaths).reduce((acc, [indexName, {pk, sk}]) => ({...acc, [indexName]: {
-        pk: 
-        lodash.get(documentSchema.parse(document), pk) as Get<
-          BaseType,
-          Path
-        >,
-      sk: 
-        sk ? lodash.get(documentSchema.parse(document), sk) as Get<
-          BaseType,
-          Path
-        > : undefined,
-      }}), {}) : undefined
-      }),
-      
-  })});
+const buildTableKey = (paths, document) =>
+(new URLSearchParams(
+  paths.reduce(
+    (acc, path) => ({ ...acc,
+      [path]: lodash.get(document, path)
+    }),
+    {},
+  )).toString()
+)
 
-  // ERROR: gsi2 should bot be allowed here
-  createTable(1).createSchema(z.object({userName: z.string(), age: z.number()}), {pk: "userName", secondaryIndexPaths: {gsi1: {pk: "age"}, gsi2: {pk: "age"}}})
-
+export const createTable = <
+  GlobalSecondaryIndices extends 0 | 1 | 2 | 3 | 4 | 5,
+>(
+  globalSecondaryIndices: GlobalSecondaryIndices,
+) => ({
+  createSchema: <
+    S extends z.ZodObject<any>,
+    BaseType extends z.infer<S>,
+    Path extends Exclude<Paths<BaseType>, number>,
+  >({
+    documentSchema,
+    keyPaths,
+  }: {
+    documentSchema: S;
+    keyPaths: {
+      pk: Path[];
+      sk?: Path[];
+      secondaryIndices?: {
+        [gsiName in AllowedGSIs[typeof globalSecondaryIndices]]?: {
+          pk: Path[];
+          sk?: Path[];
+        };
+      };
+    };
+  }) => ({
+    makeEntry: (rawDocument: BaseType) => {
+      const document = documentSchema.parse(rawDocument);
+      return {
+        document,
+        pk: buildTableKey(keyPaths.pk, document),
+        sk: keyPaths.sk
+          ? buildTableKey(keyPaths.sk, document)
+          : undefined,
+        secondaryIndices: keyPaths.secondaryIndices
+          ? (
+              Object.entries(keyPaths.secondaryIndices) as [
+                string,
+                { pk: Path; sk?: Path },
+              ][]
+            ).reduce(
+              (secondaryIndexKeys, [gsiName, { pk, sk }]) => ({
+                ...secondaryIndexKeys,
+                [gsiName]: {
+                  pk: buildTableKey(pk, document),
+                  sk: sk
+                    ? buildTableKey(sk, document)
+                    : undefined,
+                },
+              }),
+              {},
+            )
+          : undefined,
+      };
+    },
+  }),
+});
--- packages/test/index.ts
+++ packages/test/index.ts
@@ -1,1 +1,26 @@
-import "@ddb-datalayer/lib";
+import { createTable } from "@ddb-datalayer/lib";
+import { z } from "zod";
+
+const table = createTable(1)
+
+const transactionDocumentSchema = z.object({
+    userId: z.string(),
+    transactionId: z.string(),
+    transactionTags: z.array(z.string()),
+    transactionAmount: z.number(),
+    transactionTimestamp: z.number()
+});
+
+const transactionSchema = table.createSchema({
+    documentSchema: transactionDocumentSchema, 
+    keyPaths: {pk: ["userId"], sk: ["transactionTimestamp"], secondaryIndices: {gsi1: {pk: ["userId"], sk: ["transactionAmount"]}}}
+})
+
+transactionSchema.makeEntry({
+    userId: "paul",
+    transactionId: "12345",
+    transactionAmount: 100,
+    transactionTags: ["grocery"],
+    transactionTimestamp: Date.now()
+})
+
